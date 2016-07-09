820b17611b126b16b6598ac3b2be6bc87ae7d6ac
mahendiran43 <kmahee000@gmail.com>
2016-07-09 16:00:43 +0530

    finish

---
+++ README.java
@@ -0,0 +1,16 @@
+# even
+import java.io.*;
+class pig
+{
+pudlic static void main(string[]args)
+{
+int num;
+if(num%2==0)
+{
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
+}
